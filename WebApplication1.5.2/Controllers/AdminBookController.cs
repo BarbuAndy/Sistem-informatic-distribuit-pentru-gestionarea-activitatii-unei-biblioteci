e8d6b86f7b5b3f41b325d7a29ac772f97730e885
BarbuAndy <56580013+BarbuAndy@users.noreply.github.com>
2021-04-15 22:09:14 +0300

    1.5.2

---
+++ WebApplication1.5.2/Controllers/AdminBookController.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebApplication.Services;
+using WebApplication.Models;
+using javax.jws;
+using WebApplication.context;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Nancy.Json;
+
+namespace WebApplication.Controllers
+{
+    public class AdminBookController : Controller
+    {
+        private readonly BookService _bookService;
+        private readonly AuthorService _authorService;
+
+        public AdminBookController(BookService BookService,AuthorService AuthorService)
+        {
+            _bookService = BookService;
+            _authorService = AuthorService;
+        }
+
+        public ActionResult AddBook()
+        {
+            
+            List<Author> authors = _authorService.GetAuthorByCondition(b => b.AuthorId > 0);
+            List<string> AuthorNames = new List<string>();
+            foreach (var author in authors)
+            {
+                AuthorNames.Add(author.Name);
+            }
+            var jsonString = new JavaScriptSerializer().Serialize(AuthorNames);
+            System.IO.File.WriteAllText(@"D:\test.json", jsonString);
+            ViewData["authors"] = jsonString;
+            return View();
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult AddBook(BookModel Book)
+        {
+
+            try
+            {
+                _bookService.AddBook(Book);
+                _bookService.Save();
+                ModelState.Clear();
+                ViewData["Message"] = "1";
+            }
+            catch
+            {
+                ViewData["Message"] = "0";
+            }
+            return View();
+
+        }
+
+
+        public ActionResult RemoveBook()
+        {
+            return View();
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public ActionResult RemoveBook(BookModel Book)
+        {
+            try
+            {
+                _bookService.DeleteBook(Book);
+                _bookService.Save();
+                ModelState.Clear();
+                ViewData["Message"] = "1";
+            }
+            catch
+            {
+                ViewData["Message"] = "0";
+            }
+            return View();
+
+        }
+
+
+
+        public ActionResult EditBook()
+        {
+            return View();
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+
+        public ActionResult EditBook(BookModel Book)
+        {
+            try
+            {
+                _bookService.UpdateBook(Book);
+                _bookService.Save();
+                ModelState.Clear();
+                ViewData["Message"] = ViewData["Message"] + "1";
+            }
+            catch
+            {
+                ViewData["Message"] = ViewData["Message"] + "0";
+            }
+            return View();
+        }
+
+            
+     
+    }
+
+
+}
---
+++ WebApplication1.5.2/DataModels/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.context
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public int AuthorId { get; set; }
+        public string Genre { get; set; }
+        public Author Author { get; set; }
+    }
+}
