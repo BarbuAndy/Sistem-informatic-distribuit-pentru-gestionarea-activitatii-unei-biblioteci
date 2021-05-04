7327ef0521b6282357fd46fa45d3d8f215ad0750
IonutLupu00 <ionutlupu5000@gmail.com>
2021-05-04 18:47:32 +0300

    Am inceput home index view. Este nevoie de database seeding ca sa se poate afisa ceva.

--- WebApplication/WebApplication/Controllers/HomeController.cs
+++ WebApplication/WebApplication/Controllers/HomeController.cs
@@ -1,26 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Olympia_Library.Services;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using WebApplication.Models;
+using WebApplication.Repositories;
+using WebApplication.Services;
 
 namespace WebApplication.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly BookService _bookService;
+        private readonly HomeService _homeService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, BookService bookService, HomeService homeService)
         {
             _logger = logger;
+            _bookService = bookService;
+            _homeService = homeService;
         }
 
         public IActionResult Index()
-        {
-            return View();
+        {         
+            return View(_homeService.BuildHomeIndexModel());
         }
 
         public IActionResult Privacy()
---
+++ WebApplication/WebApplication/Services/HomeService.cs
@@ -0,0 +1,42 @@
+﻿using Olympia_Library.Models.BookModel;
+using Olympia_Library.Models.HomeModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication.Repositories;
+using WebApplication.Services;
+
+namespace Olympia_Library.Services
+{
+    public class HomeService :BaseService
+    {
+        private readonly BookService _bookService;
+
+        public HomeService(IRepositoryWrapper repositoryWrapper, BookService bookService) : base(repositoryWrapper)
+        {
+            _bookService = bookService;
+        }
+
+        public HomeIndexModel BuildHomeIndexModel()
+        {
+            var books = _bookService.GetAll();
+
+            var bookListing = books.Select(b => new BookListingModel
+            {
+                Title = b.Title,
+                Author = b.Author,
+                ImageUrl = b.ImageUrl,
+                Genre = b.Genre
+            });
+
+            var genres = repositoryWrapper.GenreRepository.FindByCondition(g => string.IsNullOrEmpty(g.Name));
+            return new HomeIndexModel {
+                BookListing = bookListing,
+                Genres = genres
+            };
+
+        }
+
+    }
+}
