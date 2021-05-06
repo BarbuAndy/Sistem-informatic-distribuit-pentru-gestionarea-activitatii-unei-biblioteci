33d4ed86987a16b0eeed85a5e9405fb5c60d2bb4
IonutLupu00 <ionutlupu5000@gmail.com>
2021-05-06 21:07:46 +0300

    Embedded google maps in about

---
+++ WebApplication/WebApplication/Data/Migrations/20210506143904_Azure update.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Olympia_Library.Data.Migrations
+{
+    public partial class Azureupdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
