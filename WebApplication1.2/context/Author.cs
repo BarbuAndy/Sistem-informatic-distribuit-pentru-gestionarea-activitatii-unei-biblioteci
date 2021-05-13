c5dc20e7df1185e51f1778829850893867f5c796
BarbuAndy <56580013+BarbuAndy@users.noreply.github.com>
2021-05-13 09:56:14 +0300

    tests added

--- WebApplication1.2/context/Author.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebApplication.context
-{
-    public class Author
-    {
-        public string AuthorId { get; set; }
-        public string Name { get; set; }
-    }
-}
--- WebApplication1.4/DataModels/Author.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WebApplication.context
-{
-    public class Author
-    {
-        public int AuthorId { get; set; }
-        public string Name { get; set; }
-    }
-}
